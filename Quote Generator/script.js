f379e1ea4b7102e5353c80b9a4ae3096e3bd5e1b
GabrielErens <erensoosthuizen3@gmail.com>
2022-09-28 12:01:13 +0200

    working prototype

---
+++ Quote Generator/script.js
@@ -0,0 +1,51 @@
+// Get Quotes From API
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+let apiQuotes = [];
+
+// Gets Quotes From Api
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// Show new quote
+function newQuote() {
+  // Pick random quote from API Quotes Array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // Check if Author fiels has value
+  if (quote.author === null) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+}
+
+// Tweet Quote
+function tweetQuote(){
+    const twitterUrl =`https://twitter.com/intent/tweet?text=${quoteText.textContent} 
+    - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event listeners
+newQuoteBtn.addEventListener('click', getQuotes);
+twitterBtn.addEventListener('click',tweetQuote);
+// On Load
+getQuotes();
